3aa1c26e65d7496bba010587e9716caa0959ce41
bartzbeielstein <bartzbeielstein@gmail.com>
2024-08-08 23:38:18 +0200

    slurm examples

---
+++ gui/userExperiment/startPython.py
@@ -0,0 +1,13 @@
+import argparse
+import pickle
+from spotPython.utils.file import load_and_run_spot_python_experiment
+
+def main(pickle_file):
+    spot_tuner = load_and_run_spot_python_experiment(pickle_file)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process a pickle file.')
+    parser.add_argument('pickle_file', type=str, help='The path to the pickle file to be processed.')
+
+    args = parser.parse_args()
+    main(args.pickle_file)
---
+++ gui/userExperiment/startSlurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+### Vergabe von Ressourcen
+#SBATCH --job-name=CH10_Test
+#SBATCH --ntasks-per-node=64
+#SBATCH --gres=gpu:1
+#SBATCH --time=8:00:00
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
+#----
+#SBATCH --partition=gpu
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <path_to_spot.pkl>"
+    exit 1
+fi
+
+SPOT_PKL=$1
+
+module load conda
+
+conda activate spotCondaEnv
+
+python startPython.py "$SPOT_PKL"
